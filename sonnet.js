0d3ec1a0cb882860215a8825605ddc368255c0bf
marcdledda <marcdl636@gmail.com>
2018-01-19 12:29:01 -0600

    finish [5-7]

--- sonnet.js
+++ sonnet.js
@@ -8,3 +8,5 @@ console.log(orphanIndex);
 var orphanLastIndex = sonnet.lastIndexOf(".");
 console.log(orphanLastIndex);
 
+document.getElementById("sonnet").innerHTML = document.getElementById("sonnet").innerHTML.replace("winter", "yuletide");
+document.getElementById("sonnet").innerHTML = document.getElementById("sonnet").innerHTML.replace(/the/gi, "a large");
